50f08d90a5dba48b6793f6dbf9c4d99d370cae18
Artur <artur.anissimov@gmail.com>
2020-02-19 13:53:40 +0200

    Activity Indicator

--- GCD/SecondViewController.swift
+++ GCD/SecondViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL? /* Ссылка на изображение */
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        }
+        
+        set {
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.contentMode = .scaleAspectFill
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        
+        guard let url = imageURL, let imageData = try? Data(contentsOf: url) else { return }
+        self.image = UIImage(data: imageData)
+        
     }
     
 }
